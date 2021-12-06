df78c43c0a46d765e0d8e2b76fb5e4f597876921
Russel <emkil.russel@gmaiil.com>
2021-12-06 16:20:11 +0500

    fix file location

---
+++ SoraUI/Classes/PinView/NumpadButtonStyle.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+/**
+ *  Struct is designed to represent numpad button style
+ **/
+public struct NumpadButtonStyle {
+    public let fillColor: UIColor?
+
+    public let highlightedFillColor: UIColor?
+
+    public let titleColor: UIColor?
+
+    public let titleFont: UIFont?
+
+    public let highlightedTitleColor: UIColor?
+
+    public let shadowOpacity: Float?
+
+    public let shadowColor: UIColor?
+
+    public let shadowRadius: CGFloat?
+
+    public let shadowOffset: CGSize?
+}
