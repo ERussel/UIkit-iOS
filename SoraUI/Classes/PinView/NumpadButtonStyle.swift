876558646bfcc7cb14c04681489bc230df3c97f4
Russel <emkil.russel@gmaiil.com>
2021-12-06 16:28:34 +0500

    add constructor for button style

--- SoraUI/Classes/PinView/NumpadButtonStyle.swift
+++ SoraUI/Classes/PinView/NumpadButtonStyle.swift
@@ -21,4 +21,26 @@ public struct NumpadButtonStyle {
     public let shadowRadius: CGFloat?
 
     public let shadowOffset: CGSize?
+
+    public init(
+        fillColor: UIColor? = nil,
+        highlightedFillColor: UIColor? = nil,
+        titleColor: UIColor? = nil,
+        titleFont: UIFont? = nil,
+        highlightedTitleColor: UIColor? = nil,
+        shadowOpacity: Float? = nil,
+        shadowColor: UIColor? = nil,
+        shadowRadius: CGFloat? = nil,
+        shadowOffset: CGSize? = nil
+    ) {
+        self.fillColor = fillColor
+        self.highlightedFillColor = highlightedFillColor
+        self.titleColor = titleColor
+        self.titleFont = titleFont
+        self.highlightedTitleColor = highlightedTitleColor
+        self.shadowOpacity = shadowOpacity
+        self.shadowColor = shadowColor
+        self.shadowRadius = shadowRadius
+        self.shadowOffset = shadowOffset
+    }
 }
